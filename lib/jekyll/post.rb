e3e2a1b2e5349803cdcf8c97d37d6ae0c07870f0
Nacho Martín <nitram.ohcan@gmail.com>
2011-12-17 18:54:33 +0100

    Posts will be written to disk ending in .html in multiviews

--- lib/jekyll/post.rb
+++ lib/jekyll/post.rb
@@ -199,6 +199,7 @@ module Jekyll
       # The url needs to be unescaped in order to preserve the correct filename
       path = File.join(dest, CGI.unescape(self.url))
       path = File.join(path, "index.html") if template[/\.html$/].nil?
+      path = site.config['multiviews'] ? path+'.html' : path
       path
     end
 
